11674329f4343ae6949fc9a8763ae91efb5ed940
ewsgit <ewsgit@ewsgit.uk>
2025-09-21 20:55:23 +0100

    add discord

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -12,29 +12,35 @@ const AboutPage: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <PageHeader title={"About Me"} />
-      <div className={styles.main}>
-        <Heading level={2} text={"Hiya 👋, Im Ewsgit"} />
-        <Text text={"I'm a UK based hobbyist web developer"} />
-        {/*<Text text={"I work on a few projects, such as YourDash and it's various applications"}/>*/}
-        <Button
-          onClick={() => {
-            navigate("/projects");
-          }}
-          text={"View all of my projects here"}
-        />
-        <Heading level={2} text={"Socials"} />
-        <Flex direction={"row"}>
-          <Text text={"Email:"} />
-          <Link to={"mailto:ewsgit@ewsgit.uk"} text={"ewsgit@ewsgit.uk"} />
-        </Flex>
-        <Flex direction={"row"}>
-          <Text text={"Github:"} />
-          <Link to={"https://github.com/ewsgit"} text={"Ewsgit"} />
-        </Flex>
-      </div>
-    </>
+      <>
+          <PageHeader title={"About Me"} />
+          <div className={styles.main}>
+              <Heading level={2} text={"Hiya 👋, Im Ewsgit"} />
+              <Text text={"I'm a UK based hobbyist web developer"} />
+              {/*<Text text={"I work on a few projects, such as YourDash and it's various applications"}/>*/}
+              <Button
+                  onClick={() => {
+                      navigate("/projects");
+                  }}
+                  text={"View all of my projects here"}
+              />
+              <Heading level={2} text={"Socials"} />
+              <Flex direction={"row"}>
+                  <Text text={"Email:"} />
+                  <Link
+                      to={"mailto:ewsgit@ewsgit.uk"}
+                      text={"ewsgit@ewsgit.uk"}
+                  />
+              </Flex>
+              <Flex direction={"row"}>
+                  <Text text={"Github:"} />
+                  <Link to={"https://github.com/ewsgit"} text={"Ewsgit"} />
+              </Flex>
+              <Flex direction={"row"}>
+                  <Text text={"Discord: @ewsgit"} />
+              </Flex>
+          </div>
+      </>
   );
 };
 
